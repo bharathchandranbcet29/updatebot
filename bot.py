4b4f75432dbc29286e8c8b92195eeb81c63785c2
Bharath Chandran B <bharathchandranb2006@gmail.com>
2026-06-13 14:27:00 +0530

    Add automatic portfolio updater

--- bot.py
+++ bot.py
@@ -1,33 +1,51 @@
 import requests
 import json
+import os
+import base64
 
 USERNAME = "bharathchandranbcet29"
 
+# Generate projects.json
 url = f"https://api.github.com/users/{USERNAME}/repos"
-
 repos = requests.get(url).json()
 
 projects = []
 
 for repo in repos:
-
     projects.append(
         {
             "title": repo["name"],
-            "description": repo["description"],
+            "description": repo["description"] or "GitHub Automation Project",
             "github": repo["html_url"]
         }
     )
 
-with open(
-    "projects.json",
-    "w"
-) as file:
+json_content = json.dumps(projects, indent=4)
 
-    json.dump(
-        projects,
-        file,
-        indent=4
-    )
+# Update portfolio repository
+TOKEN = os.getenv("PAT_TOKEN")
 
-print("projects.json generated successfully!")
+headers = {
+    "Authorization": f"token {TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+repo_api = "https://api.github.com/repos/bharathchandranbcet29/portfolio/contents/projects.json"
+
+# Get current file SHA
+current = requests.get(repo_api, headers=headers).json()
+
+payload = {
+    "message": "Auto update projects.json",
+    "content": base64.b64encode(json_content.encode()).decode(),
+    "sha": current["sha"]
+}
+
+response = requests.put(
+    repo_api,
+    headers=headers,
+    json=payload
+)
+
+print(response.status_code)
+print("Portfolio projects.json updated!")
